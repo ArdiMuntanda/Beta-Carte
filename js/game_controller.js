46c0ce0231d5ed79961953c0692d571fa4f56476
Ardi Muntanda <ardimuntanda1@gmail.com>
2022-02-14 03:02:34 -0800

    delete the old tries

--- js/game_controller.js
+++ js/game_controller.js
@@ -4,21 +4,6 @@ showCards();
 detectPlay();
 turnTick()
 // turn management
-document.querySelector(".turn_tracker").addEventListener("change", function (e) {
-        let target = e.target;
-        console.log(target);
-        if (target.value == 0 && kopo.turn == 0) {
-            turnTick();
-            detectPlay();
-            wipeTable();
-        } else if (target.value == 1 && kopo.turn == 1) {
-            turnTick();
-            setTimeout(ordiPlay, 3000);
-            wipeTable();
-            document.querySelector(".turn_tracker").value = 0;        
-        }
-});
-
 function turnManager () {
     if (kopo.turn == 0) {
         turnTick();
